c6addaba57b2ac5f080f8ccb240db5c197b187e8
Rocio Batres <jonasgirl21@gmail.com>
2020-09-15 02:44:14 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,14 +1,11 @@
 require 'pry'
 
 def my_all?(collection)
-  if block_given?  
     i = 0 
     while i < collection.length
       yield(collection[i])
       i += 1 
     end
-  else
-    "Please call a block!"
   end
   my_all(collection)
 end
